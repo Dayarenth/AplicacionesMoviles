04b8dba3644c7b2a8809c1eeaa8aa2ef4b463cb5
Emi Atiencie <58490441+Remilio11@users.noreply.github.com>
2021-03-15 14:33:01 -0500

    Descripcion de Pelicula - Emi Atiencie

--- KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/LoginActivity.kt
+++ KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/LoginActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
 
         var bt_continuar = findViewById<Button>(R.id.btnRegistrar)
         bt_continuar.setOnClickListener {
