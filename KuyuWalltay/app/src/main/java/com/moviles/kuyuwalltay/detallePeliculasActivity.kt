6a4585462f73326792cfda7616efdd7de65d298b
Emi Atiencie <58490441+Remilio11@users.noreply.github.com>
2021-03-15 01:36:20 -0500

    Descripcion de Pelicula - Emi Atiencie

---
+++ KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/detallePeliculasActivity.kt
@@ -0,0 +1,11 @@
+package com.moviles.kuyuwalltay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detallePeliculasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_peliculas)
+    }
+}
