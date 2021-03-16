995bdb7b45e74a2c89f2f3b00bd2f62ca4f96151
Emi Atiencie <58490441+Remilio11@users.noreply.github.com>
2021-03-15 22:37:07 -0500

    Pantalla de Cines  - Emi Atiencie

---
+++ KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/PeliculasModelClass.kt
@@ -0,0 +1,4 @@
+package com.moviles.kuyuwalltay
+
+data class PeliculasModelClass
+    (var userId:Int, var nombrePelicula: String, var precioPelicula: Int, var descripcionPelicula: String, var estrellasPelicula: Int)
--- KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/PrincipalActivity.kt
+++ KuyuWalltay/app/src/main/java/com/moviles/kuyuwalltay/PrincipalActivity.kt
@@ -2,10 +2,22 @@ package com.moviles.kuyuwalltay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PrincipalActivity : AppCompatActivity() {
+    var peliculas = arrayListOf<PeliculasModelClass>()
+    var selectedContactPosition = 0
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        db = FirebaseFirestore.getInstance()
+        consultarPelicula()
+    }
+
+    fun consultarPelicula (){
+
     }
 }
